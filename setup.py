1547aa0385a979bd4d5b3d5af004e7a51fb37e85
Dale Jung <dale@dalejung.com>
2014-10-01 03:51:48 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+DISTNAME='zouyu'
+FULLVERSION='0.1'
+
+setup(
+    name=DISTNAME,
+    version=FULLVERSION,
+    packages=['zouyu'],
+    entry_points={
+    }
+      )
